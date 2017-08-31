2b0ccc5be1aad066d73deba95b56945c925016fc
Nat Wright <natalee.wright413@gmail.com>
2017-08-30 21:04:03 -0600

    Added Script

---
+++ UnityProject001/Assets/Mammal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mammal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Eat();
+        Sleep();
+	}
+	
+	void Eat ()
+    {
+        print(this.name + " Eats");
+    }
+
+    void Sleep ()
+    {
+        print(this.name + " Sleeps");
+    }
+}
